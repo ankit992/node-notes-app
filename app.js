2eb89e7709904ce8f17916b5335aae1c82b2e935
Ankit Arora <ankit992@live.in>
2017-10-23 21:28:04 +0530

    connecting app.js to notes.js and defining our console api for the notes application

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
-var res = notes.add(5, 2);
+const yargs = require('yargs');
+const notes = require('./notes.js');
+const argv = yargs.argv;
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log('Yargs', argv);
 
-console.log(res);
-// var user = os.userInfo();
-
-// console.log(user.username)
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} \n`);
+if(command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();    
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recogized');
+}
