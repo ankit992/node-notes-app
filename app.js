836e40898738648ce10a39a475c545d9aac06a89
Ankit Arora <ankit992@live.in>
2017-10-23 21:30:51 +0530

    switching command to yargs instead of process yargs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const yargs = require('yargs');
 const notes = require('./notes.js');
 const argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs', argv);
 
