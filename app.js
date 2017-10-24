f62a90571918c1f4a0f0148e22f2b8f1b8f3a3b7
Ankit Arora <ankit992@live.in>
2017-10-24 21:02:32 +0530

    advanced features in yargs for command alias and required

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const _ = require('lodash');
 const yargs = require('yargs');
 const notes = require('./notes.js');
-const argv = yargs.argv;
+
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new Note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list','List all notes')
+    .command('read','Read a note', {
+        title: titleOptions
+    })
+    .command('remove','Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 var command = argv._[0];
 
 if(command === 'add') {
@@ -12,7 +38,6 @@ if(command === 'add') {
     } else {
         console.log('note title taken');
     } 
-
 } else if (command === 'list') {
     var allNotes = notes.getAll();
     console.log(`Printing ${allNotes.length} note(s).`);
